c494089e58cfc963abc5ee889a9c7d0d3af1a6c2
Pepe Becker <mail@pepebecker.com>
2017-04-24 20:25:47 +0200

    Add mocha tests

--- test/index.js
+++ test/index.js
@@ -2,26 +2,65 @@
 
 const split = require('../index')
 
-split('wǒdemāoxǐhuānhēniúnǎi').then((data) => {
-	console.log('Test 1:', data.join(' '))
-}, console.error)
+describe('Split text without spaces', () => {
+	it('should split the text into the correct words', () => {
+		return split('wodemaoxihuanheniunai').then((data) => {
+			const list = ['wo', 'de', 'mao', 'xi', 'huan', 'he', 'niu', 'nai']
+			data.should.deepEqual(list)
+		})
+	})
+	it('should split the text into the correct words', () => {
+		return split('wo3demao1xi3huan1he2niu3nai3').then((data) => {
+			const list = ['wo3', 'de', 'mao1', 'xi3', 'huan1', 'he2', 'niu3', 'nai3']
+			data.should.deepEqual(list)
+		})
+	})
+	it('should split the text into the correct words', () => {
+		return split('wǒdemāoxǐhuānhéniǔnǎi').then((data) => {
+			const list = ['wǒ', 'de', 'māo', 'xǐ', 'huān', 'hé', 'niǔ', 'nǎi']
+			data.should.deepEqual(list)
+		})
+	})
+})
 
-split('wo3demao1xi3huan1he1niu2nai3').then((data) => {
-	console.log('Test 2:', data.join(' '))
-}, console.error)
+describe('Split spaced text without keeping spaces', () => {
+	it('should split the text into the correct words', () => {
+		return split('wo de mao xihuan he niunai').then((data) => {
+			const list = ['wo', 'de', 'mao', 'xi', 'huan', 'he', 'niu', 'nai']
+			data.should.deepEqual(list)
+		})
+	})
+	it('should split the text into the correct words', () => {
+		return split('wo3 de mao1 xi3huan1 he2 niu3nai3').then((data) => {
+			const list = ['wo3', 'de', 'mao1', 'xi3', 'huan1', 'he2', 'niu3', 'nai3']
+			data.should.deepEqual(list)
+		})
+	})
+	it('should split the text into the correct words', () => {
+		return split('wǒ de māo xǐhuān hé niǔnǎi').then((data) => {
+			const list = ['wǒ', 'de', 'māo', 'xǐ', 'huān', 'hé', 'niǔ', 'nǎi']
+			data.should.deepEqual(list)
+		})
+	})
+})
 
-split('wodemaoxihuanheniunai').then((data) => {
-	console.log('Test 3:', data.join(' '))
-}, console.error)
-
-split('wǒ de māo xǐhuān hē niúnǎi', {keepSpaces: true}).then((data) => {
-	console.log('Test 4:', data.join(''))
-}, console.error)
-
-split('wo3 de mao1 xi3huan1 he1 niu2nai3', {keepSpaces: true}).then((data) => {
-	console.log('Test 5:', data.join(''))
-}, console.error)
-
-split('wo de mao xihuan he niunai', {keepSpaces: true}).then((data) => {
-	console.log('Test 6:', data.join(''))
-}, console.error)
+describe('Split spaced text and keep spaces', () => {
+	it('should split the text into the correct words', () => {
+		return split('wo de mao xihuan he niunai', {keepSpaces: true}).then((data) => {
+			const list = ['wo', ' ', 'de', ' ', 'mao', ' ', 'xi', 'huan', ' ', 'he', ' ', 'niu', 'nai']
+			data.should.deepEqual(list)
+		})
+	})
+	it('should split the text into the correct words', () => {
+		return split('wo3 de mao1 xi3huan1 he2 niu3nai3', {keepSpaces: true}).then((data) => {
+			const list = ['wo3', ' ', 'de', ' ', 'mao1', ' ', 'xi3', 'huan1', ' ', 'he2', ' ', 'niu3', 'nai3']
+			data.should.deepEqual(list)
+		})
+	})
+	it('should split the text into the correct words', () => {
+		return split('wǒ de māo xǐhuān hé niǔnǎi', {keepSpaces: true}).then((data) => {
+			const list = ['wǒ', ' ', 'de', ' ', 'māo', ' ', 'xǐ', 'huān', ' ', 'hé', ' ', 'niǔ', 'nǎi']
+			data.should.deepEqual(list)
+		})
+	})
+})
