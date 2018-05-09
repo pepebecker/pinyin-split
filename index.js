7aabfb6b1470268a90de57d9fe4aa09a9f0a3b21
Pepe Becker <mail@pepebecker.com>
2018-05-09 02:05:30 +0100

    Improve and simplify split algorythm

--- index.js
+++ index.js
@@ -1,121 +1,45 @@
 'use strict'
 
-const wordsData = require('./words.json')
 const utils = require('pinyin-utils')
+const wordsData = require('./words.json')
 
-let allWords = []
+let allWords = Object.values(wordsData).reduce((o, i) => o.concat(i), [])
 
-for (let wordList in wordsData) {
-	allWords = allWords.concat(wordsData[wordList])
-}
-
-// Add all four tones
-for (var i = 1; i <= 4; i++) {
-	allWords = allWords.concat(allWords.map((word) => utils.numberToMark(word + i)))
-}
-
-// Add capitalized words
-allWords = allWords.concat(allWords.map((word) => utils.capitalize(word)))
-
-// Sort them by having the longest ones on the top
-allWords = allWords.sort((a, b) => b.length - a.length)
-
-let doNotMatch = []
-let recursionCount = 0
-
-const inDoNotMatch = (index, word) => {
-  for (let item of doNotMatch) {
-    if (item.index === index && item.word === word) {
-      return true
-    }
+const wordsCount = allWords.length
+for (let i = 0; i < wordsCount; i++) {
+  for (let n = 1; n <= 5; n++) {
+    allWords.push(utils.numberToMark(allWords[i] + n))
+    allWords.push(allWords[i] + n)
   }
-  return false
 }
 
-const reAllowed = /^[a-züāáǎàēéěèīíǐìōóǒòūúǔùǖǘǚǜ]+$/i
-
-const matchWords = (text, keepSpaces, rc, debug) => {
-  if (debug) {
-		console.log(`DEBUG> Recursion count: '${recursionCount}'`)
-		console.log(`DEBUG> do not match: '${doNotMatch.map((item) => item.index + ':' + item.word).join()}'`)
-	}
-
-  let words = []
-  let i = 0
-
-  while (i < text.length) {
+const split = (text, opts = {}) => {
+  const list = []
+  let wordEnd = text.length
+  while (wordEnd > 0) {
+    let count = wordEnd
     let wordFound = false
-
-    if (!reAllowed.test(text[i])) {
-			if (keepSpaces) words.push({index: i, word: text[i]})
-      i ++
-      wordFound = true
-    }
-
-    for (let word of allWords) {
-      if (inDoNotMatch(i, word)) {
-        if (debug) console.log('DEBUG> Skipped:', word)
-        continue
-      }
-
-      if (text.substr(i, word.length) === word) {
-        if (debug) console.log('DEBUG> Found word:', word)
-
-        const item = {index: i, word: word}
-        i += word.length
-
-        if (i < text.length) {
-          const tone = text[i]
-          if (/[1-5]/.test(tone)) {
-            if (debug) console.log('DEBUG> Found tone:', tone)
-            item.tone = tone
-            i ++
-          }
-        }
-
-        words.push(item)
+    while (count > 1) {
+      const word = text.substring(wordEnd - count, wordEnd)
+      if (allWords.includes(word.toLowerCase())) {
         wordFound = true
+        list.push(opts.onlyPinyin ? word : [word])
+        wordEnd -= (count - 1)
         break
       }
+      count--
     }
-
-    if (!wordFound) {
-      const lastItem = words[words.length - 1]
-      if (lastItem) {
-        doNotMatch.push(lastItem)
-        if (debug) console.log('DEBUG> Adding word to doNotMatch list:', lastItem.word)
-        if (rc > 0) {
-          recursionCount ++
-          return matchWords(text, --rc, debug)
-        }
-        if (debug) console.log(`DEBUG> Max recursion count exceeded`)
-        return null
-      } else {
-        if (debug) console.log('DEBUG> Error on line 94!')
-        return null
+    if (!wordFound && !opts.onlyPinyin) {
+      if (wordEnd === text.length || typeof list[list.length - 1] === 'object') {
+        list.push(text[wordEnd - 1])
+      }
+      else if (typeof list[list.length - 1] === 'string') {
+        list[list.length - 1] = text[wordEnd - 1] + list[list.length - 1]
       }
     }
+    wordEnd --
   }
-
-  if (debug) console.log(`DEBUG> Text was sucessfully splitted`)
-  return words
+  return list.reverse()
 }
-
-const split = (text, options = {}) => new Promise((yay, nay) => {
-  doNotMatch = []
-
-	const keepSpaces = options.keepSpaces || false
-	const maxRecursion = options.maxRecursion || 5
-	const debug = options.debug || false
-
-  let words = matchWords(text, keepSpaces, maxRecursion, debug)
-
-  if (words) {
-    words = words.map((item) => item.word + (item.tone?item.tone:''))
-    yay(words)
-  } else {
-    nay('Failed to split words')
-  }
-})
 
 module.exports = split
--- test/index.js
+++ test/index.js
@@ -2,83 +2,71 @@
 
 const split = require('../index')
 
-describe('Split text without spaces', () => {
-	it('should split the text into the correct words', () => {
-		return split('wodemaoxihuanheniunai').then((data) => {
-			const list = ['wo', 'de', 'mao', 'xi', 'huan', 'he', 'niu', 'nai']
-			data.should.deepEqual(list)
-		})
+describe('Split text and keep non-pinyin text', () => {
+	it('should split the text into the correct words', done => {
+		const list = [['wo'], ['de'], ['mao'], ['xi'], ['huan'], ['he'], ['niu'], ['nai']]
+		split('wodemaoxihuanheniunai').should.deepEqual(list)
+		done()
 	})
-	it('should split the text into the correct words', () => {
-		return split('wo3demao1xi3huan1he2niu3nai3').then((data) => {
-			const list = ['wo3', 'de', 'mao1', 'xi3', 'huan1', 'he2', 'niu3', 'nai3']
-			data.should.deepEqual(list)
-		})
+	it('should split the text into the correct words', done => {
+		const list = [['wo3'], ['de'], ['mao1'], ['xi3'], ['huan1'], ['he2'], ['niu3'], ['nai3']]
+		split('wo3demao1xi3huan1he2niu3nai3').should.deepEqual(list)
+		done()
 	})
-	it('should split the text into the correct words', () => {
-		return split('wǒdemāoxǐhuānhéniǔnǎi').then((data) => {
-			const list = ['wǒ', 'de', 'māo', 'xǐ', 'huān', 'hé', 'niǔ', 'nǎi']
-			data.should.deepEqual(list)
-		})
+	it('should split the text into the correct words', done => {
+		const list = [['wǒ'], ['de'], ['māo'], ['xǐ'], ['huān'], ['hé'], ['niǔ'], ['nǎi']]
+		split('wǒdemāoxǐhuānhéniǔnǎi').should.deepEqual(list)
+		done()
 	})
-	it('should split the text into the correct words', () => {
-		return split('derewofendoudouworenwei').then((data) => {
-			const list = ['de', 're', 'wo', 'fen', 'dou', 'dou', 'wo', 'ren', 'wei']
-			data.should.deepEqual(list)
-		})
+	it('should split the text into the correct words', done => {
+		const list = [['de'], ['re'], ['wo'], ['fen'], ['dou'], ['dou'], ['wo'], ['ren'], ['wei']]
+		split('derewofendoudouworenwei').should.deepEqual(list)
+		done()
 	})
-	it('should split the text into the correct words', () => {
-		return split('de2re3wo3fen4dou4dou3wo3ren4wei4').then((data) => {
-			const list = ['de2', 're3', 'wo3', 'fen4', 'dou4', 'dou3', 'wo3', 'ren4', 'wei4']
-			data.should.deepEqual(list)
-		})
+	it('should split the text into the correct words', done => {
+		const list = [['de2'], ['re3'], ['wo3'], ['fen4'], ['dou4'], ['dou3'], ['wo3'], ['ren4'], ['wei4']]
+		split('de2re3wo3fen4dou4dou3wo3ren4wei4').should.deepEqual(list)
+		done()
 	})
-	it('should split the text into the correct words', () => {
-		return split('dérěwǒfèndòudǒuwǒrènwèi').then((data) => {
-			const list = ['dé', 'rě', 'wǒ', 'fèn', 'dòu', 'dǒu', 'wǒ', 'rèn', 'wèi']
-			data.should.deepEqual(list)
-		})
+	it('should split the text into the correct words', done => {
+		const list = [['dé'], ['rě'], ['wǒ'], ['fèn'], ['dòu'], ['dǒu'], ['wǒ'], ['rèn'], ['wèi']]
+		split('dérěwǒfèndòudǒuwǒrènwèi').should.deepEqual(list)
+		done()
 	})
 })
 
-describe('Split spaced text without keeping spaces', () => {
-	it('should split the text into the correct words', () => {
-		return split('wo de mao xihuan he niunai').then((data) => {
-			const list = ['wo', 'de', 'mao', 'xi', 'huan', 'he', 'niu', 'nai']
-			data.should.deepEqual(list)
-		})
+describe('Split text and return Pinyin only', () => {
+	it('should split the text into the correct words', done => {
+		const list = ['wo', 'de', 'mao', 'xi', 'huan', 'he', 'niu', 'nai']
+		split('wo de mao xihuan he niunai', { onlyPinyin: true }).should.deepEqual(list)
+		done()
 	})
-	it('should split the text into the correct words', () => {
-		return split('wo3 de mao1 xi3huan1 he2 niu3nai3').then((data) => {
-			const list = ['wo3', 'de', 'mao1', 'xi3', 'huan1', 'he2', 'niu3', 'nai3']
-			data.should.deepEqual(list)
-		})
+	it('should split the text into the correct words', done => {
+		const list = ['wo3', 'de', 'mao1', 'xi3', 'huan1', 'he2', 'niu3', 'nai3']
+		split('wo3 de mao1 xi3huan1 he2 niu3nai3', { onlyPinyin: true }).should.deepEqual(list)
+		done()
 	})
-	it('should split the text into the correct words', () => {
-		return split('wǒ de māo xǐhuān hé niǔnǎi').then((data) => {
-			const list = ['wǒ', 'de', 'māo', 'xǐ', 'huān', 'hé', 'niǔ', 'nǎi']
-			data.should.deepEqual(list)
-		})
+	it('should split the text into the correct words', done => {
+		const list = ['wǒ', 'de', 'māo', 'xǐ', 'huān', 'hé', 'niǔ', 'nǎi']
+		split('wǒ de māo xǐhuān hé niǔnǎi', { onlyPinyin: true }).should.deepEqual(list)
+		done()
 	})
 })
 
 describe('Split spaced text and keep spaces', () => {
-	it('should split the text into the correct words', () => {
-		return split('wo de mao xihuan he niunai', {keepSpaces: true}).then((data) => {
-			const list = ['wo', ' ', 'de', ' ', 'mao', ' ', 'xi', 'huan', ' ', 'he', ' ', 'niu', 'nai']
-			data.should.deepEqual(list)
-		})
+	it('should split the text into the correct words', done => {
+		const list = [['wo'], ' ', ['de'], ' ', ['mao'], ' ', ['xi'], ['huan'], ' ', ['he'], ' ', ['niu'], ['nai']]
+		split('wo de mao xihuan he niunai', {keepSpaces: true}).should.deepEqual(list)
+		done()
 	})
-	it('should split the text into the correct words', () => {
-		return split('wo3 de mao1 xi3huan he2 niu3nai3', {keepSpaces: true}).then((data) => {
-			const list = ['wo3', ' ', 'de', ' ', 'mao1', ' ', 'xi3', 'huan', ' ', 'he2', ' ', 'niu3', 'nai3']
-			data.should.deepEqual(list)
-		})
+	it('should split the text into the correct words', done => {
+		const list = [['wo3'], ' ', ['de'], ' ', ['mao1'], ' ', ['xi3'], ['huan'], ' ', ['he2'], ' ', ['niu3'], ['nai3']]
+		split('wo3 de mao1 xi3huan he2 niu3nai3', {keepSpaces: true}).should.deepEqual(list)
+		done()
 	})
-	it('should split the text into the correct words', () => {
-		return split('wǒ de māo xǐhuan hé niǔnǎi', {keepSpaces: true}).then((data) => {
-			const list = ['wǒ', ' ', 'de', ' ', 'māo', ' ', 'xǐ', 'huan', ' ', 'hé', ' ', 'niǔ', 'nǎi']
-			data.should.deepEqual(list)
-		})
+	it('should split the text into the correct words', done => {
+		const list = [['wǒ'], ' ', ['de'], ' ', ['māo'], ' ', ['xǐ'], ['huan'], ' ', ['hé'], ' ', ['niǔ'], ['nǎi']]
+		split('wǒ de māo xǐhuan hé niǔnǎi', {keepSpaces: true}).should.deepEqual(list)
+		done()
 	})
 })
